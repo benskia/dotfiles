dc4640d3c6bdbefb3d1d512c7d147d0abc120577
distrollo <AbrahamBenski@proton.me>
2025-05-03 04:29:24 -0500

    update hypr, scripts

--- installToHome.sh
+++ installToHome.sh
@@ -4,13 +4,13 @@ declare -A toIgnore
 
 # Add item names to ignore here
 for name in .git .gitignore README.md installToHome.sh pullFromHome.sh; do
-	toIgnore["$name"]=1
+  toIgnore["$name"]=1
 done
 
 for item in * .*; do
-	if [[ ${toIgnore["$item"]} ]]; then
-		continue
-	fi
-	echo "Copying $item  =>  $HOME$item"
-	cp -r "$item" $HOME
+  if [[ ${toIgnore["$item"]} ]]; then
+    continue
+  fi
+  echo "Copying $item  =>  $HOME/$item"
+  cp -r "$item" $HOME
 done
